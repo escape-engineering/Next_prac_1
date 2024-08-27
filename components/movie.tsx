e0a703fe88e979d7ee4bf4c2831911266a8bd013
escape-engineering <tkdrnr1215@naver.com>
2024-08-27 10:13:26 +0900

    Suspense까지

---
+++ components/movie.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import styles from "../styles/movie.module.css";
+import { useRouter } from "next/navigation";
+
+interface IMovieProps {
+    title: string;
+    id: string;
+    poster_path: string;
+}
+
+const Movie = ({ poster_path, title, id }: IMovieProps) => {
+    const router = useRouter();
+    const onClick = () => {
+        router.push(`/movies/${id}`);
+    };
+    return (
+        <div className={styles.movie}>
+            <img src={poster_path} alt={title} onClick={onClick} />
+            <Link href={`/movie/${id}`}>{title}</Link>
+        </div>
+    );
+};
+
+export default Movie;
